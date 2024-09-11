e723fea6cca7c9ec6b65fc2473021c314d373cee
JOAO PEDRO OLIVEIRA JORDAO <161616023+jao905@users.noreply.github.com>
2024-09-11 13:38:00 +0000

    [fix] Create Payments Datable

--- src/database/initializeDatabase.js
+++ src/database/initializeDatabase.js
@@ -1,11 +1,14 @@
 export async function initializeDatabase(database) {
-    try {
-        await database.execAsync(`
+  try {
+    await database.execAsync(`
+            DROP TABLE IF EXISTS payments;
+
             DROP TABLE IF EXISTS users;
 
             CREATE TABLE IF NOT EXISTS users (
              id INTEGER PRIMARY KEY AUTOINCREMENT,
              nome TEXT,
+             curso TEXT,
              email TEXT NOT NULL UNIQUE,
              senha TEXT NOT NULL DEFAULT 'A123456a!',
              role TEXT NOT NULL DEFAULT 'USER',
@@ -13,11 +16,24 @@ export async function initializeDatabase(database) {
              updated_at DATE
             );
 
+            CREATE TABLE IF NOT EXISTS payments (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            user_cadastro INTEGER NOT NULL,
+            valor_pago REAL NOT NULL,
+            data_pagamento DATE NOT NULL,
+            observacao TEXT,
+            created_at DATE DEFAULT CURRENT_TIMESTAMP,
+             updated_at DATE,
+             FOREIGN KEY (user_id) REFERENCES users(id),
+             FOREIGN KEY (user_cadastro) REFERENCES users(id)
+            );
+
             INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('Super', 'super@email.com', 'A123456a!', 'SUPER');
             INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('Admin', 'admin@email.com', 'A123456a!', 'ADMIN');
             INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('User', 'user@email.com', 'A123456a!', 'USER');
             `);
-    } catch (error) {
-        console.log(error);
-    }
+  } catch (error) {
+    console.log(error);
+  }
 }
